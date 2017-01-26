09b01b98969bfcc41a13816c8d876e0fd0c1b94e
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-01-26 14:40:55 +0300

    Add spec examples for both module and class inflection testing

--- spec/shared_examples/inflection.rb
+++ spec/shared_examples/inflection.rb
@@ -1,11 +1,15 @@
 RSpec.shared_examples 'inflection' do |answers|
   describe answers[0] do
-    let(:inflection_object) { RussianInflect.new(answers[0]) }
+    let(:inflector) { RussianInflect.new(answers[0]) }
 
     RussianInflect::CASES.each_with_index do |inflection_case, index|
       context "with case #{inflection_case}" do
-        it 'inflects properly' do
-          expect(inflection_object.to_case(inflection_case)).to eq(answers[index])
+        it 'inflects through inflector' do
+          expect(inflector.to_case(inflection_case)).to eq(answers[index])
+        end
+
+        it 'inflects through module method' do
+          expect(RussianInflect.inflect(answers[0], inflection_case)).to eq(answers[index])
         end
       end
     end
