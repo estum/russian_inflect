c760818d3c22e688bbc12381ff49f0bff8ffd7f1
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-07-05 17:23:40 +0300

    Move cases constants to separated file

--- lib/russian_inflect.rb
+++ lib/russian_inflect.rb
@@ -1,17 +1,10 @@
 require 'yaml'
 require 'unicode_utils'
+require 'russian_inflect/cases'
 require 'russian_inflect/rules'
 require 'russian_inflect/version'
 
 class RussianInflect
-  NOMINATIVE      = :nominative    # именительный
-  GENITIVE        = :genitive      # родительный
-  DATIVE          = :dative        # дательный
-  ACCUSATIVE      = :accusative    # винительный
-  INSTRUMENTAL    = :instrumental  # творительный
-  PREPOSITIONAL   = :prepositional # предложный
-
-  CASES = [NOMINATIVE, GENITIVE, DATIVE, ACCUSATIVE, INSTRUMENTAL, PREPOSITIONAL].freeze
   GROUPS = [nil, :first, :second, :third].freeze
 
   attr_accessor :source, :words, :noun, :case_group
---
+++ lib/russian_inflect/cases.rb
@@ -0,0 +1,10 @@
+class RussianInflect
+  NOMINATIVE    = :nominative    # именительный
+  GENITIVE      = :genitive      # родительный
+  DATIVE        = :dative        # дательный
+  ACCUSATIVE    = :accusative    # винительный
+  INSTRUMENTAL  = :instrumental  # творительный
+  PREPOSITIONAL = :prepositional # предложный
+
+  CASES = [NOMINATIVE, GENITIVE, DATIVE, ACCUSATIVE, INSTRUMENTAL, PREPOSITIONAL].freeze
+end
